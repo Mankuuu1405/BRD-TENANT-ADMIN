eafb97e0b2d4ace0c62cba2163b68adc81b7794f
root <root@ip-172-31-45-109.ap-south-1.compute.internal>
2025-12-15 15:07:38 +0000

    added .env axios vite files

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // .env फाइल से बेस URL लें (जैसे: http://127.0.0.1:8000)
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://13.234.113.42:8000';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
